d92889f17938c6b8808ae7ecef0512d87e0e3b2d
LOUISL <laetitia.louis.lp4@gmail.com>
2020-03-21 19:18:10 +0100

    components utiles page et accueil HTML et CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,33 @@ import {ExtendedModule, FlexModule} from '@angular/flex-layout';
 import {RouterModule} from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './page/navbar/navbar.component';
+import { PageComponent } from './page/page.component';
+import { FooterComponent } from './page/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PrestationComponent } from './prestation/prestation.component';
+import { ProjetComponent } from './projet/projet.component';
+import { ContactComponent } from './contact/contact.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 const routes: any[] = [
-  {path: '', component: AppComponent}
+  {path: '', component: AccueilComponent},
+  {path: 'prestations', component: PrestationComponent},
+  {path: 'projets', component: ProjetComponent},
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PageComponent,
+    FooterComponent,
+    AccueilComponent,
+    PrestationComponent,
+    ProjetComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +40,10 @@ const routes: any[] = [
     ExtendedModule,
     RouterModule.forRoot(routes),
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
